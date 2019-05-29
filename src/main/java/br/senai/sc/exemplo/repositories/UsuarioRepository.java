b267aad975dba7ca296a911dd2aad1aa1f2516d3
Disraeli Stubert <33765601+disraeli-stubert@users.noreply.github.com>
2019-05-28 22:14:02 -0300

    Tratamento de exceptions

--- src/main/java/br/senai/sc/exemplo/repositories/UsuarioRepository.java
+++ src/main/java/br/senai/sc/exemplo/repositories/UsuarioRepository.java
@@ -1,10 +1,19 @@
 package br.senai.sc.exemplo.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.senai.sc.exemplo.domain.Usuario;
 
 public interface UsuarioRepository extends 
-	CrudRepository<Usuario, Long>{
+	CrudRepository<Usuario, Long> {
+	
+	Optional<Usuario> findByCpf(String cpf);
+	
+	@Query(value="SELECT u FROM Usuario u WHERE u.email LIKE %:email%")
+	Optional<Usuario> buscarPorEmail(@Param("email") String email);
 
 }
--- src/main/java/br/senai/sc/exemplo/service/UsuarioService.java
+++ src/main/java/br/senai/sc/exemplo/service/UsuarioService.java
@@ -1,9 +1,12 @@
 package br.senai.sc.exemplo.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.senai.sc.exemplo.domain.Usuario;
+import br.senai.sc.exemplo.exceptions.CustomRuntimeException;
 import br.senai.sc.exemplo.repositories.UsuarioRepository;
 
 @Service
@@ -13,6 +16,14 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepository;
 	
 	public Usuario salvar(Usuario usuario) {
+		Optional<Usuario> usuarioOptional = this.usuarioRepository.findByCpf(usuario.getCpf());
+		if (usuarioOptional.isPresent()) {
+			throw new CustomRuntimeException("cpf", "CPF já cadastrado!");
+		}
+		usuarioOptional = this.usuarioRepository.buscarPorEmail(usuario.getEmail());
+		if (usuarioOptional.isPresent()) {
+			throw new CustomRuntimeException("email", "E-mail já cadastrado!");
+		}
 		return this.usuarioRepository.save(usuario);
 	}
 	
